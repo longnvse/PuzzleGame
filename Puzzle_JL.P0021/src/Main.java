30de29be81bde2ddcbc64d552f9dc65f3f922833
Long Híp <85206935+longnvse@users.noreply.github.com>
2021-08-11 21:32:02 +0700

    puzzle game

---
+++ Puzzle_JL.P0021/src/Main.java
@@ -0,0 +1,23 @@
+
+import javax.swing.JPanel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author longhip
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Controller ctl = new Controller();
+    }
+
+}
